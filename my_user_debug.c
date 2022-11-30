2efdd358b015892431fe30f007c129947de42dc2
Ana <ana.mrzv@mail.ru>
2022-11-30 23:51:18 +0300

    fix

--- my_user_debug.c
+++ my_user_debug.c
@@ -59,19 +59,20 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    printf("%d",fd);
+    printf("%d File descryptor\n",fd);
+    printf("%d option\n", option);
 
     if (option == PCI_DEV_OPTION) {
         struct ioctl_pci_dev pci_dev = {0};
         struct pci_parameters pci_params = { &pci_dev, (uint16_t) argv[2], (uint16_t) argv[3] };
         uint8_t ret = ioctl(fd, IOCTL_GET_PCIDEV, &pci_params);
-        printf("%d", ret);
+        printf("ret code %d\n", ret);
         if (ret == 0) print_pci(&pci_dev);
     } else if (option == THREAD_STRUCT_OPTION) {
         struct ioctl_thread_struct thread = {0};
         struct thread_parameters thread_params = { &thread, (uint16_t) argv[2] };
         uint8_t ret = ioctl(fd, IOCTL_GET_THREADSTRUCT, &thread_params);
-        printf("%d", ret);
+        printf("ret code%d\n", ret);
         if (ret == 0) print_thread(&thread);
     }
 
