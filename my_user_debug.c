c490077e1a644d79da96cd738c573871be487a3d
Ana <ana.mrzv@mail.ru>
2022-12-05 17:24:50 +0300

    return

--- my_user_debug.c
+++ my_user_debug.c
@@ -78,30 +78,47 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    struct pci_parameters* pci_params;
-    struct thread_parameters* thread_params;
+    // struct pci_parameters* pci_params;
+    // struct thread_parameters* thread_params;
 
-    struct ioctl_pci_dev pci_dev = {0};
-    struct ioctl_thread_struct thread = {0};
+    // struct ioctl_pci_dev pci_dev;
+    // struct ioctl_thread_struct thread;
 
-    switch (option) {
-        case PCI_DEV_OPTION:
-            init_pci_params(&pci_dev, first, second, pci_params);
-            if (ioctl(fd, IOCTL_GET_PCIDEV, &pci_params)) print_pci(&pci_dev);
-            else printf("Failed to get info from kernel. Check if the parameters are right.\n");
-            free(pci_params->write_pointer);
-            free(pci_params);
-            break;
-        case THREAD_STRUCT_OPTION:
-            init_thread_params(&thread, first, thread_params);
-            if (ioctl(fd, IOCTL_GET_THREADSTRUCT, &thread_params)) print_thread(&thread);
-            else printf("Failed to get info from kernel. Check if the parameters are right.\n");
-            free(thread_params->write_pointer);
-            free(thread_params);
-            break;
-        default:
-            printf("No such option\n");
-            break;
+    // switch (option) {
+    //     case PCI_DEV_OPTION:
+    //         init_pci_params(&pci_dev, first, second, pci_params);
+    //         if (ioctl(fd, IOCTL_GET_PCIDEV, &pci_params)) print_pci(&pci_dev);
+    //         else printf("Failed to get info from kernel. Check if the parameters are right.\n");
+    //         free(pci_params->write_pointer);
+    //         free(pci_params);
+    //         break;
+    //     case THREAD_STRUCT_OPTION:
+    //         init_thread_params(&thread, first, thread_params);
+    //         if (ioctl(fd, IOCTL_GET_THREADSTRUCT, &thread_params)) print_thread(&thread);
+    //         else printf("Failed to get info from kernel. Check if the parameters are right.\n");
+    //         free(thread_params->write_pointer);
+    //         free(thread_params);
+    //         break;
+    //     default:
+    //         printf("No such option\n");
+    //         break;
+    // }
+
+    if (option == PCI_DEV_OPTION) {
+        struct ioctl_pci_dev pci_dev;
+        uint32_t vendor = strtoul(argv[2], NULL, 16);
+        uint32_t device = strtoul(argv[3], NULL, 16);
+        struct pci_parameters pci_params = { .write_pointer = &pci_dev, .vendor = vendor, .device = device };
+        uint8_t ret = ioctl(fd, IOCTL_GET_PCIDEV, &pci_params);
+        if (ret == 0) print_pci(&pci_dev);
+        else printf("Failed to get info from kernel. Check if the parameters are right.\n");
+    } else if (option == THREAD_STRUCT_OPTION) {
+        struct ioctl_thread_struct thread;
+        uint32_t pid = strtoul(argv[2], NULL, 10);
+        struct thread_parameters thread_params = { .write_pointer = &thread, .pid = pid };
+        uint8_t ret = ioctl(fd, IOCTL_GET_THREADSTRUCT, &thread_params);
+        if (ret == 0) print_thread(&thread);
+        else printf("Failed to get info from kernel. Check if the parameters are right.\n");
     }
 
     return 0;
